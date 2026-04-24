ba1548c9a0452a04eaea52b12894e349d5dbb5e0
Matheus ferreira trindade <vantrindade.matheus@gmail.com>
2026-04-23 23:34:45 -0300

    Update Partidas-Rankeadas.js

--- Partidas-Rankeadas.js
+++ Partidas-Rankeadas.js
@@ -1,24 +1,31 @@
-function vitoriasDerrotas(vitorias,derrotas) {
-  let nivel;
+function vitoriasDerrotas(nome, vitorias, derrotas) {
+    let nivel;
+
     if (vitorias < 10) nivel = "Ferro";
     else if (vitorias <= 20) nivel = "Bronze";
     else if (vitorias <= 50) nivel = "Prata";
     else if (vitorias <= 80) nivel = "Ouro";
     else if (vitorias <= 90) nivel = "Diamante";
-    else if (vitorias <= 100) nivel = "Lendario";
+    else if (vitorias <= 100) nivel = "Lendário";
     else nivel = "Imortal";
-    
-    let rankeadas = vitorias - derrotas
 
-    return `O heroi tem saldo de ${rankeadas} ${nivel}`;
+    let saldo = vitorias - derrotas;
+
+    return `O Herói ${nome} tem de saldo de ${saldo} está no nível de ${nivel}`;
 }
 
 let jogadores = [
     { nome: "Kratos", vitorias: 120, derrotas: 10 },
-    { nome: "Link", vitorias: 35, derrotas: 16},
-    { nome: "Ezio", vitorias: 80, derrotas: 0}
+    { nome: "Link", vitorias: 35, derrotas: 16 },
+    { nome: "Ezio", vitorias: 80, derrotas: 0 }
 ];
 
 for (let i = 0; i < jogadores.length; i++) {
-    console.log(vitoriasDerrotas(jogadores[i].vitorias, jogadores[i].derrotas ));
+    console.log(
+        vitoriasDerrotas(
+            jogadores[i].nome,
+            jogadores[i].vitorias,
+            jogadores[i].derrotas
+        )
+    );
 }
